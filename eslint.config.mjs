31ae1ab0aa47a68d758a4ff607bf7b407cfbbd6b
zgildo01 <zzaragozagildo@gmail.com>
2026-09-07 16:42:01 -0400

    configuring typescript

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import js from '@eslint/js';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import tseslint from 'typescript-eslint';
+
+export default [
+    {
+        ignores: [
+            'dist/**',
+            '.astro/**',
+            'node_modules/**',
+        ],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...eslintPluginAstro.configs.recommended,
+];
